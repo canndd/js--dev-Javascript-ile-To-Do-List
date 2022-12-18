f64566eae59881d624a00d3771f720b21cf2caa5
Can DEMİR <cannddemirr@gmail.com>
2022-12-18 21:42:52 +0300

    Ul içerisinde li ekleme işlemi sağlandı

---
+++ js/javascript.js
@@ -0,0 +1,20 @@
+var liste = document.querySelector("#list");
+
+
+var urun = document.querySelector("#task"); 
+var ekle = document.querySelector("#liveToastBtn");
+
+
+ekle.addEventListener("click",function(){
+    // createElement ile yeni bir listItem (li) oluşturuyoruz
+    var li=document.createElement("li"); 
+    
+    // Input ile gelen veriyi "li" içerisine metin olarak giriyoruz.
+    li.textContent=task.value; 
+    
+    // appendChild ile oluşturduğumuz elementi "liste" isimli değişkenimizin içerisine ekliyoruz.
+    liste.appendChild(li);
+                
+    // Veri inputu içerisindeki metni siliyoruz.
+    veri.value = "";
+  });
